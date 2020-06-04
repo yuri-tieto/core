125e73c57d697e2230d40b662c0eb5f9c1479e18
Yuri Barseghyan <yuri.barseghyan@tieto.com>
2020-06-04 14:44:11 +0300

    added ssh security rule

--- main.tf
+++ main.tf
@@ -18,6 +18,19 @@ resource "azurerm_network_security_group" "main_nsg" {
   name                = "main-nsg"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    description                = "Allow SSH"
+  }
 }
 
 resource "azurerm_virtual_network" "main_vnet" {
